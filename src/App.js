a6e62b39d7ecbf1cafae20a421076371c55538ed
far5had <hackyourfuture@hackyouturesair.home>
2023-09-27 16:01:08 +0200

    added Root route

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-// Add react-router-dom imports
-
-// create router with JSX Route elements
-const appRouter = "REPLACE ME";
 
 function App() {
   return (
-    // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <Router >
+      <Routes>
+        <Route path='/' element={<Root />} />
+      </Routes>
+    </Router>
   );
 }
 
